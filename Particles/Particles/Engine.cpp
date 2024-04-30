2c52acfe177908166d223f4c73e9ed225b51ccdf
Damian Saelee <damian4lif@yahoo.com>
2024-04-30 16:04:22 -0700

    Added some stuff to engine

--- Particles/Particles/Engine.cpp
+++ Particles/Particles/Engine.cpp
@@ -1,1 +1,38 @@
 #include "Engine.h"
+
+void Engine::input() {
+	Event event;
+
+	while (this->game->window.pollEvent(event)) {
+		switch (event.type) {
+		case Event::Closed: {
+			this->game->window.close();
+			break;
+		}
+		case Event::KeyPressed:
+			if (event.key.code == Keyboard::Escape) {
+				this->game->window.close();
+				break;
+			}
+			// Spaceholder if we plan to add more screens.
+			if (event.key.code == Keyboard::Space) {
+				cout << "loaded" << endl;
+				break;
+			}
+		case Event::MouseButtonPressed: {
+			if (event.mouseButton.button == Mouse::Left) {
+				cout << "Left mouse button was pressed" << endl;
+				cout << "mouse x: " << event.mouseButton.x << endl;
+				cout << "mouse y: " << event.mouseButton.y << endl;
+			}
+			break;
+		}
+									  break;
+		}
+			default: break;
+	}
+}
+
+void Engine::run() {
+	Clock clock;
+}
